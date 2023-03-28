caebafb4a092a7a537e814b40324dc8e5c96f992
tajon1030 <da33202@gmail.com>
2023-03-28 17:26:19 +0900

    Feat: setting project

---
+++ src/test/java/com/beside/ten011/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beside.ten011;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
