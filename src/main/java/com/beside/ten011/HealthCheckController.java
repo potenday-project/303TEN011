954dbafa028e5dfe2ee61dd873715161e2bb6230
tajon1030 <da33202@gmail.com>
2023-03-29 21:05:13 +0900

    Feat: 헬스체크 api 추가

---
+++ src/main/java/com/beside/ten011/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.beside.ten011;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/hcheck")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<Long> healthCheck() {
+        return ResponseEntity.ok(System.currentTimeMillis());
+    }
+}
